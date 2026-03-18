82e7f1540c6a2840ee078ffb590ed17c19f687be
its_jedu <olajideweb@gmail.com>
2026-03-18 16:04:29 +0100

    feat: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4F46E5",
+        accent: "#6366F1",
+        secondary: "#FBBF24",
+        neutral: "#F9FAFB",
+        textPrimary: "#111827",
+      },
+    },
+  },
+  plugins: [],
+};
